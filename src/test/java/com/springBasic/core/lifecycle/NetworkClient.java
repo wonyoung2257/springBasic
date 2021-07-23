36c9e64d0be2b0feaf4de149c84682c20def5292
Won-young <dnjsdud2257@gmail.com>
2021-07-23 16:10:53 +0900

    Bean LifeCycle

--- src/test/java/com/springBasic/core/lifecycle/NetworkClient.java
+++ src/test/java/com/springBasic/core/lifecycle/NetworkClient.java
@@ -1,14 +1,16 @@
 package com.springBasic.core.lifecycle;
 
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-    }
+        }
 
     public void setUrl(String url) {
         this.url = url;
@@ -25,5 +27,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
